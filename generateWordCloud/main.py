3a9bce5d65f85f6bb29d393a030528124bc3d931
ggqshr <942490944@qq.com>
2018-07-20 11:17:10 +0800

    图片备份

--- generateWordCloud/main.py
+++ generateWordCloud/main.py
@@ -3,11 +3,15 @@
 # 作用：生成青岛旅游景点的词图云
 # 获取每个景点的关键词，前30个
 import pymysql
-import wordcloud as wc
+from wordcloud import WordCloud, ImageColorGenerator
+from scipy.misc import imread
+import matplotlib.pyplot as plt
 import os
-for _,_,x in os.walk("./npic"):
+
+for _, _, x in os.walk("./pic"):
     pass
 allfile = [z.decode("gbk") for z in x]
+
 
 def getData(cur, emotion, place):
     sql = ''
@@ -22,7 +26,7 @@ def getData(cur, emotion, place):
               "where emotion='负面' and comment_place=%s " \
               "order by countNum desc"
     cur.execute(sql, (place))
-    data = cur.fetchmany(50)
+    data = cur.fetchmany(100)
     dd = {}
     for i in data:
         dd[i[0]] = i[1]
@@ -37,17 +41,19 @@ def getName(cur):
     return [x[0] for x in lines]
 
 
-ww = wc.WordCloud(background_color='white',
+bimg = imread("./circle.png")
+ww = WordCloud(background_color='white',
                   font_path="simfang.ttf",
                   random_state=60,
-                  scale=1.5
+                  mask=bimg
                   )
 
 
 # 绘制词图云
 def drawPic(data, place):
     ww.generate_from_frequencies(data)
-    ww.to_file("./npic/" + place + ".png")
+    image_colors = ImageColorGenerator(bimg)
+    ww.to_file("./pic/" + place + ".png")
 
 
 if __name__ == '__main__':
@@ -61,10 +67,12 @@ if __name__ == '__main__':
     )
     place_line = getName(conn.cursor())
     for place in place_line:
-        if place+".png" in allfile:
+        if place + ".png" in allfile:
             print "跳过了", place, "的图片"
             continue
-        place_key_word = getData(conn.cursor(), "负面", place)
+        place_key_word = getData(conn.cursor(), "正面", place)
+        if place_key_word.__len__()<50:
+            continue
         try:
             drawPic(place_key_word, place)
         except ValueError as e:
